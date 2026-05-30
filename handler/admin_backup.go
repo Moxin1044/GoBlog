38285c4f50f6e9862a9e0306a6ddac01e3390ce0
末心 <1044631097@qq.com>
2026-05-30 09:24:25 +0800

    refactor(admin_backup): 替换mysqldump为原生Go实现数据库备份
    将原有的外部mysqldump命令备份方式改为纯Go原生实现，不再依赖外部命令，提升备份的兼容性和稳定性，同时增加了备份文件的标准SQL头注释和格式校验。

--- handler/admin_backup.go
+++ handler/admin_backup.go
@@ -1,10 +1,10 @@
 package handler
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"time"
 
@@ -13,9 +13,9 @@ import (
 	"github.com/moxin/GoBlog/database"
 	"github.com/moxin/GoBlog/model"
 	"github.com/moxin/GoBlog/utils"
+	"gorm.io/gorm"
 )
 
-// GetBackupList 获取备份列表
 func GetBackupList(c *gin.Context) {
 	var backups []model.Backup
 	if err := database.DB.Order("created_at DESC").Find(&backups).Error; err != nil {
@@ -25,39 +25,99 @@ func GetBackupList(c *gin.Context) {
 	responseSuccess(c, backups)
 }
 
-// CreateBackup 创建备份
 func CreateBackup(c *gin.Context) {
 	adminID := c.GetUint("admin_id")
 
-	cfg := config.AppConfig.Database
 	timestamp := time.Now().Format("20060102_150405")
 	filename := fmt.Sprintf("goblog_backup_%s.sql", timestamp)
 	backupDir := filepath.Join(config.AppConfig.Upload.Path, "backups")
-
-	// 确保备份目录存在
 	os.MkdirAll(backupDir, 0755)
-
 	backupPath := filepath.Join(backupDir, filename)
 
-	// 使用mysqldump命令导出
-	cmd := exec.Command("mysqldump",
-		"-h", cfg.Host,
-		"-P", fmt.Sprintf("%d", cfg.Port),
-		"-u", cfg.User,
-		fmt.Sprintf("-p%s", cfg.Password),
-		cfg.DBName,
-		"-r", backupPath,
-	)
-
-	output, err := cmd.CombinedOutput()
+	f, err := os.Create(backupPath)
 	if err != nil {
-		responseErrorWithCode(c, http.StatusInternalServerError, "备份失败: "+string(output))
+		responseErrorWithCode(c, http.StatusInternalServerError, "备份失败: "+err.Error())
 		return
 	}
+	defer f.Close()
 
-	// 获取文件大小
-	var fileInfo os.FileInfo
-	fileInfo, _ = os.Stat(backupPath)
+	w := bufio.NewWriter(f)
+	w.WriteString(fmt.Sprintf("-- GoBlog Backup\n-- Date: %s\n-- Database: %s\n\n",
+		time.Now().Format("2006-01-02 15:04:05"), config.AppConfig.Database.DBName))
+	w.WriteString("SET NAMES utf8mb4;\nSET FOREIGN_KEY_CHECKS = 0;\n\n")
+
+	tables := []interface{}{
+		&model.User{}, &model.Article{}, &model.Category{}, &model.Tag{}, &model.ArticleTag{},
+		&model.Comment{}, &model.Like{}, &model.Subscription{}, &model.SubscriptionCategory{},
+		&model.AIModel{}, &model.UserAIConfig{}, &model.ChatMessage{}, &model.SiteConfig{},
+		&model.Admin{}, &model.OperationLog{}, &model.VerificationCode{}, &model.VisitLog{},
+		&model.Navigation{},
+	}
+
+	for _, table := range tables {
+		stmt := &gorm.Statement{DB: database.DB}
+		stmt.Parse(table)
+		tableName := stmt.Schema.Table
+
+		w.WriteString(fmt.Sprintf("-- ----------------------------\n-- Table structure for `%s`\n-- ----------------------------\n", tableName))
+		w.WriteString(fmt.Sprintf("DROP TABLE IF EXISTS `%s`;\n", tableName))
+
+		var createSQL string
+		var tblName string
+		database.DB.Raw(fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName)).Row().Scan(&tblName, &createSQL)
+		w.WriteString(createSQL + ";\n\n")
+
+		w.WriteString(fmt.Sprintf("-- ----------------------------\n-- Records of `%s`\n-- ----------------------------\n", tableName))
+
+		rows, err := database.DB.Table(tableName).Rows()
+		if err != nil {
+			rows.Close()
+			continue
+		}
+
+		columns, _ := rows.Columns()
+		for rows.Next() {
+			values := make([]interface{}, len(columns))
+			valuePtrs := make([]interface{}, len(columns))
+			for i := range columns {
+				valuePtrs[i] = &values[i]
+			}
+			rows.Scan(valuePtrs...)
+
+			colNames := ""
+			colValues := ""
+			for i, col := range columns {
+				if i > 0 {
+					colNames += ", "
+					colValues += ", "
+				}
+				colNames += fmt.Sprintf("`%s`", col)
+				val := values[i]
+				if val == nil {
+					colValues += "NULL"
+				} else {
+					switch v := val.(type) {
+					case []byte:
+						colValues += fmt.Sprintf("'%s'", escapeString(string(v)))
+					case string:
+						colValues += fmt.Sprintf("'%s'", escapeString(v))
+					case time.Time:
+						colValues += fmt.Sprintf("'%s'", v.Format("2006-01-02 15:04:05"))
+					default:
+						colValues += fmt.Sprintf("'%v'", v)
+					}
+				}
+			}
+			w.WriteString(fmt.Sprintf("INSERT INTO `%s` (%s) VALUES (%s);\n", tableName, colNames, colValues))
+		}
+		rows.Close()
+		w.WriteString("\n")
+	}
+
+	w.WriteString("SET FOREIGN_KEY_CHECKS = 1;\n")
+	w.Flush()
+
+	fileInfo, _ := os.Stat(backupPath)
 	var fileSize int64
 	if fileInfo != nil {
 		fileSize = fileInfo.Size()
@@ -76,7 +136,29 @@ func CreateBackup(c *gin.Context) {
 	responseSuccess(c, backup)
 }
 
-// DownloadBackup 下载备份
+func escapeString(s string) string {
+	result := ""
+	for _, c := range s {
+		switch c {
+		case '\'':
+			result += "\\'"
+		case '\\':
+			result += "\\\\"
+		case '\n':
+			result += "\\n"
+		case '\r':
+			result += "\\r"
+		case '\t':
+			result += "\\t"
+		case 0:
+			result += "\\0"
+		default:
+			result += string(c)
+		}
+	}
+	return result
+}
+
 func DownloadBackup(c *gin.Context) {
 	id := c.Param("id")
 
@@ -97,7 +179,6 @@ func DownloadBackup(c *gin.Context) {
 	c.File(backupPath)
 }
 
-// DeleteBackup 删除备份
 func DeleteBackup(c *gin.Context) {
 	id := c.Param("id")
 	adminID := c.GetUint("admin_id")
@@ -108,7 +189,6 @@ func DeleteBackup(c *gin.Context) {
 		return
 	}
 
-	// 删除文件
 	backupPath := filepath.Join(config.AppConfig.Upload.Path, "backups", backup.Filename)
 	os.Remove(backupPath)
 
@@ -120,7 +200,6 @@ func DeleteBackup(c *gin.Context) {
 	responseSuccess(c, nil)
 }
 
-// UpdateAutoBackupConfig 更新自动备份配置
 func UpdateAutoBackupConfig(c *gin.Context) {
 	var req struct {
 		Enabled   bool   `json:"enabled"`
